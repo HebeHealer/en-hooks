bdefc2f48265ee271f09e2d6f4cb266044215ab5
HebeHealer <HebeHealer@163.com>
2024-01-19 07:16:13 +0000

    Deploying to gh-pages from @ HebeHealer/en-hooks@d5eaeb25488b2caae9bc268eec4fb12afc634560 🚀

--- packages/hooks/src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import * as hooks from '..';
-
-describe('en-hooks', () => {
-  test('exports modules should be defined', () => {
-    Object.keys(hooks).forEach((module) => {
-      expect(hooks[module]).toBeDefined();
-    });
-  });
-});
